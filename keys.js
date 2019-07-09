2f83a3fb3e0490fde3008b3376894e7f7a763655
Temi Olaoluniyi <tolaoluniyi@gmail.com>
2019-07-08 21:59:24 -0400

    this is a commit of the LIRI-bot app

---
+++ keys.js
@@ -0,0 +1,10 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'P2veeddblJvLpn3amZTZG0Edg',
+  consumer_secret: 'P4TIwlMY2L4uSYvcyiDXNdb7W8oLtafa1vwwSSqIGQLx6O7Px3',
+  access_token_key: '224787051-sM7jxtNTvx0fxks1XDn9a9LiM20T1XVDOHsXuoIX',
+  access_token_secret: 'M1TmZ662bOUoxyFGqp31LKjEqy59q8juCWv8n2ShJLgmJ',
+}
+
+exports.tmdbKey = '8d5f41f6535c312ee8ea248cecd733f7';
